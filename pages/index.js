63553471685bd2d141aad1a2bff40f64196c836f
Andre Parsons <andrparsons@gmail.com>
2021-02-23 16:26:18 -0500

    container component - applied  header and  home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import Container from "../components/Container/Container";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,70 +14,79 @@ export default function Home() {
 
       <main className={styles.main}>
         <section className={styles.intro}>
-          <h1>Award-winning custom designs and digital branding solutions</h1>
+          <Container>
+            <h1>Award-winning custom designs and digital branding solutions</h1>
 
-          <p>
-            With over 10 years in the industry, we are experienced in creating
-            fully responsive websites, app design, and engaging brand
-            experiences. Find out more about our services.
-          </p>
-          <button>Learn more</button>
+            <p>
+              With over 10 years in the industry, we are experienced in creating
+              fully responsive websites, app design, and engaging brand
+              experiences. Find out more about our services.
+            </p>
+            <button>Learn more</button>
+          </Container>
         </section>
 
         {/* project links s/b component */}
         <section className={styles.projectLinks}>
-          <div className={styles.project}>
-            <h2>Web Design</h2>
-            View Projects
-          </div>
+          <Container>
+            <div className={styles.project}>
+              <h2>Web Design</h2>
+              View Projects
+            </div>
 
-          <div className={styles.project}>
-            <h2>App Design</h2>
-            View Projects
-          </div>
-          <div className={styles.project}>
-            <h2>Graphic Design</h2>
-            View Projects
-          </div>
+            <div className={styles.project}>
+              <h2>App Design</h2>
+              View Projects
+            </div>
+            <div className={styles.project}>
+              <h2>Graphic Design</h2>
+              View Projects
+            </div>
+          </Container>
         </section>
 
         <section className={styles.features}>
-          <div className={styles.feature}>
-            <h3>Passionate</h3>
-            <p>
-              Each project starts with an in-depth brand research to ensure we
-              only create products that serve a purpose. We merge art, design,
-              and technology into exciting new solutions.
-            </p>
-          </div>
+          <Container>
+            <div className={styles.feature}>
+              <h3>Passionate</h3>
+              <p>
+                Each project starts with an in-depth brand research to ensure we
+                only create products that serve a purpose. We merge art, design,
+                and technology into exciting new solutions.
+              </p>
+            </div>
 
-          <div className={styles.feature}>
-            <h3>Resourceful</h3>
-            <p>
-              Everything that we do has a strategic purpose. We use an agile
-              approach in all of our projects and value customer collaboration.
-              It guarantees superior results that fulfill our clients’ needs.
-            </p>
-          </div>
+            <div className={styles.feature}>
+              <h3>Resourceful</h3>
+              <p>
+                Everything that we do has a strategic purpose. We use an agile
+                approach in all of our projects and value customer
+                collaboration. It guarantees superior results that fulfill our
+                clients’ needs.
+              </p>
+            </div>
 
-          <div className={styles.feature}>
-            <h3>Friendly</h3>
-            <p>
-              We are a group of enthusiastic folks who know how to put people
-              first. Our success depends on our customers, and we strive to give
-              them the best experience a company can provide.
-            </p>
-          </div>
+            <div className={styles.feature}>
+              <h3>Friendly</h3>
+              <p>
+                We are a group of enthusiastic folks who know how to put people
+                first. Our success depends on our customers, and we strive to
+                give them the best experience a company can provide.
+              </p>
+            </div>
+          </Container>
         </section>
 
         {/* this should be a component  */}
         <section className={styles.CTA}>
-          <h2>Let’s talk about your project</h2>
-          <p>
-            Ready to take it to the next level? Contact us today and find out
-            how our expertise can help your business grow.
-          </p>
-          <button>Get in touch</button>
+          <Container>
+            <h2>Let’s talk about your project</h2>
+            <p>
+              Ready to take it to the next level? Contact us today and find out
+              how our expertise can help your business grow.
+            </p>
+            <button>Get in touch</button>
+          </Container>
         </section>
       </main>
     </div>
