50cdc266da18b746dc03f64e6287c9c6f98e6ede
Andre Parsons <andrparsons@gmail.com>
2021-02-24 08:20:46 -0500

    mobile styling for footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import styles from "./Footer.module.css";
 import Link from "next/link";
 
+import Container from "../Container/Container";
+
 // svg imgs
 // import {
 //   IconFacebook,
@@ -11,31 +13,41 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.internalLinks}>
-        <Link href="/">
-          <a>
-            <img src="/shared/desktop/logo-light.png" />
-          </a>
-        </Link>
-        <div className={styles.footerLinks}>
-          <Link href="/about">
-            <a className={styles.footerLink}>our company</a>
+      <Container>
+        <div className={styles.internalLinks}>
+          <Link href="/">
+            <a className={styles.logoLink}>
+              <img src="/shared/desktop/logo-light.png" />
+            </a>
           </Link>
-          <Link href="/locations">
-            <a className={styles.footerLink}>locations</a>
-          </Link>
-          <Link href="/contact">
-            <a className={styles.footerLink}>contact</a>
-          </Link>
+          <div className={styles.footerLinks}>
+            <Link href="/about">
+              <a className={styles.footerLink}>our company</a>
+            </Link>
+            <Link href="/locations">
+              <a className={styles.footerLink}>locations</a>
+            </Link>
+            <Link href="/contact">
+              <a className={styles.footerLink}>contact</a>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.address}>
-        Designo Central Office 3886 Wellington Street Toronto, Ontario M9C 3J5
-        Contact Us (Central Office) P : +1 253-863-8967 M : contact@designo.co
-      </div>
+        <div className={styles.addresses}>
+          <div className={styles.address}>
+            <h4>Designo Central Office</h4>
+            <p className={styles.addressLine}>3886 Wellington Street</p>
+            <p className={styles.addressLine}>Toronto, Ontario M9C 3J5</p>
+          </div>
 
-      {/* <div className={styles.footerIcons}>
+          <div className={styles.address}>
+            <h4>Contact Us (Central Office)</h4>
+            <p className={styles.addressLine}>P : +1 253-863-8967</p>
+            <p className={styles.addressLine}>M : contact@designo.co</p>
+          </div>
+        </div>
+
+        {/* <div className={styles.footerIcons}>
         <a className={styles.iconLink} href="https://www.facebook.com">
           <IconFacebook />
         </a>
@@ -46,6 +58,7 @@ export default function Footer() {
           <IconInstagram />
         </a>
       </div> */}
+      </Container>
     </footer>
   );
 }
