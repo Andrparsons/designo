deec5276d29c9410a869c0bb1e46c1e5e733eac9
Andre Parsons <andrparsons@gmail.com>
2021-03-02 11:54:35 -0500

    index page - intro styling - mobile

--- pages/index.js
+++ pages/index.js
@@ -28,16 +28,12 @@ export default function Home() {
           <Container>
             <h1>Award-winning custom designs and digital branding solutions</h1>
 
-            <p>
+            <p className={styles.introParagraph}>
               With over 10 years in the industry, we are experienced in creating
               fully responsive websites, app design, and engaging brand
               experiences. Find out more about our services.
             </p>
             <Button>Learn more</Button>
-            <img
-              src="/home/desktop/image-hero-phone.png"
-              alt="Picture of a phone"
-            />
           </Container>
         </section>
 
