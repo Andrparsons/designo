ba4ba36f64b4688a5b34f136d0802f0440f41084
Andre Parsons <andrparsons@gmail.com>
2021-03-15 21:20:46 -0400

    added locations page text

--- pages/locations.js
+++ pages/locations.js
@@ -30,6 +30,19 @@ export default function Home() {
             </div>
             <div className={styles.locationContent}>
               <h2 className={styles.locationTitle}>Canada</h2>
+              <div className={styles.addresses}>
+                <div className={styles.address}>
+                  <h4>Designo Central Office</h4>
+                  <p className={styles.addressLine}>3886 Wellington Street</p>
+                  <p className={styles.addressLine}>Toronto, Ontario M9C 3J5</p>
+                </div>
+
+                <div className={styles.address}>
+                  <h4>Contact</h4>
+                  <p className={styles.addressLine}>P : +1 253-863-8967</p>
+                  <p className={styles.addressLine}>M : contact@designo.co</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className={styles.location}>
@@ -39,6 +52,19 @@ export default function Home() {
             </div>
             <div className={styles.locationContent}>
               <h2 className={styles.locationTitle}>Australia</h2>
+              <div className={styles.addresses}>
+                <div className={styles.address}>
+                  <h4>Designo AU Office</h4>
+                  <p className={styles.addressLine}>19 Balonne Street</p>
+                  <p className={styles.addressLine}>New South Wales 2443</p>
+                </div>
+
+                <div className={styles.address}>
+                  <h4>Contact</h4>
+                  <p className={styles.addressLine}>P : (02) 6720 9092</p>
+                  <p className={styles.addressLine}>M : contact@designo.au</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className={styles.location}>
@@ -48,6 +74,19 @@ export default function Home() {
             </div>
             <div className={styles.locationContent}>
               <h2 className={styles.locationTitle}>United Kingdom</h2>
+              <div className={styles.addresses}>
+                <div className={styles.address}>
+                  <h4>Designo UK Office</h4>
+                  <p className={styles.addressLine}>13 Colorado Way</p>
+                  <p className={styles.addressLine}>Rhyd-fro, SA8 9GA</p>
+                </div>
+
+                <div className={styles.address}>
+                  <h4>Contact</h4>
+                  <p className={styles.addressLine}>P : 078 3115 1400</p>
+                  <p className={styles.addressLine}>M : contact@designo.uk</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
